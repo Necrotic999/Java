69e21454e1531ceeb312ef02774c4e5fd94e97df
Necrotic999 <l.ilya041105@gmail.com>
2026-05-18 14:36:30 +0300

    lab8

--- Lab4/lab4/src/main/java/com/lab/lab4/MuseumController.java
+++ Lab4/lab4/src/main/java/com/lab/lab4/MuseumController.java
@@ -1,46 +1,51 @@
 package com.lab.lab4;
 
-import com.lab.lab4.Museum;
-import com.lab.lab4.MuseumService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/museums")
-@CrossOrigin(origins = "http://localhost:5173")
-@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MuseumController {
 
-    private final MuseumService service;
+    @Autowired
+    private MuseumRepository museumRepository;
 
     @GetMapping
-    public List<Museum> getAll() {
-        return service.getAllMuseums();
-    }
-
-    @GetMapping("/{id}")
-    public Museum getById(@PathVariable Long id) {
-        return service.getMuseumById(id);
+    public List<Museum> getAllMuseums() {
+        log.info("Отримання списку всіх музеїв з бази даних");
+        return museumRepository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<Museum> create(@RequestBody Museum museum) {
-        Museum saved = service.saveMuseum(museum);
-        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    public Museum createMuseum(@RequestBody Museum museum) {
+        log.info("Додавання нового музею: {}", museum.getName());
+        return museumRepository.save(museum);
     }
 
     @PutMapping("/{id}")
-    public Museum update(@PathVariable Long id, @RequestBody Museum museum) {
-        return service.updateMuseum(id, museum);
+    public Museum updateMuseum(@PathVariable Long id, @RequestBody Museum museumDetails) {
+        log.info("Оновлення інформації про музей з ID: {}", id);
+        Museum museum = museumRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Museum not found with id: " + id));
+
+        museum.setName(museumDetails.getName());
+        museum.setCity(museumDetails.getCity());
+        museum.setCountry(museumDetails.getCountry());
+        museum.setDescription(museumDetails.getDescription());
+        museum.setImageUrl(museumDetails.getImageUrl());
+        museum.setYearFounded(museumDetails.getYearFounded());
+
+        return museumRepository.save(museum);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        service.deleteMuseum(id);
-        return ResponseEntity.noContent().build();
+    public void deleteMuseum(@PathVariable Long id) {
+        log.info("Видалення музею з ID: {}", id);
+        museumRepository.deleteById(id);
     }
 }
